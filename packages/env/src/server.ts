e73beaf4fe76c925af130d1ff30ccdce9157c4a2
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-08-26 18:41:07 +0200

    initial commit

---
+++ packages/env/src/server.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+  },
+  runtimeEnv: process.env,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
